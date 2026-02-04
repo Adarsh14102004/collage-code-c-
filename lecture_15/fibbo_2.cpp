1853a32edfe9c7c17d8d86c54bb13493c01eb260
Adarsh14102004 <kalmodiyaadarsh@2004@gmail.com>
2026-02-04 13:48:53 +0530

    fiboo1

---
+++ lecture_15/fibbo_2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int fibbo(int n) {
+    if(n == 0 || n == 1) {
+        return n;
+    }
+    // recursion
+    return fibbo(n-1) + fibbo(n-2);
+}
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n-1; i++)
+    {
+        cout<<fibbo(i)<<" ";    
+    }
+    
+    return 0;
+}
