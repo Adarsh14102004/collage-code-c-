5d0c7389e5f19a6dbbb76968f283e6f936841ebb
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-05 20:47:11 +0530

    pattern22

--- lecture_5/pattern_22.cpp
+++ lecture_5/pattern_22.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> n;
     int k=1;
 
-    for(int i = 1; i <= n; i++) {
+    for(int i = 1; i<=n; i++) {
         
         for(int j = 1; j <= n - i; j++) {
             cout <<" \t";
