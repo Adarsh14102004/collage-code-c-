136773bd16c0ad9e21bbe04eb73347c93ea2ba3f
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-10 15:19:55 +0530

    Aray3

--- Array/Array4.cpp
+++ Array/Array4.cpp
@@ -5,22 +5,15 @@ int main () {
    int n;
    cin>>n;
    int arr[n];
+   int key;
    for (int i = 0; i < n; i++)
    {
     cin>>arr[i];
     
    }
-   int ans=0;
-   for (int i = 0; i <n ; i++)
-   {
-     ans+=arr[i];
-     
-   }
-   cout<<ans<<endl;
+    cin>>key;
+    
+   
    return 0;
 }
    
-   
-  
-   
-
