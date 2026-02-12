1c723b0c5860edc3615b7404ff6bfc0f43ab8ff9
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-12 20:10:42 +0530

    que13

--- Assingnment3/que12.cpp
+++ Assingnment3/que12.cpp
@@ -1,29 +1,45 @@
-// Amstrong number
-#include <iostream>
-#include <cmath>
-using namespace std;
+// // Amstrong number
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+// int main() {
+//     int n;
+//     cin >> n;
 
-    int original = n;
-    int sum = 0;
+//     int original = n;
+//     int sum = 0;
 
     
-    while (n>0)
-    {
-        int digit = n % 10;
-        sum += digit * digit * digit;
-        n /= 10;
+//     while (n>0)
+//     {
+//         int digit = n % 10; 
+//         sum += digit * digit * digit;
+//         n /= 10;
+//     }
+    
+//     // Armstrong sum nikalna
+//    cout<<sum<<" ";
+//     if (sum == original)
+//         cout << "Armstrong Number";
+//     else
+//         cout << "Not Armstrong Number";
+
+//     return 0;
+// }
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                
+                if (nums[i] + nums[j] == target) {
+                    return{i, j};
+                }
+            }
+        }
+        
+        return {}; 
     }
-    
-    // Armstrong sum nikalna
-   cout<<sum<<" ";
-    if (sum == original)
-        cout << "Armstrong Number";
-    else
-        cout << "Not Armstrong Number";
-
-    return 0;
 }
---
+++ Assingnment3/que13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int main() {
+    long long N1, N2;
+    cin >> N1;
+    cin >> N2;
+    cout << gcd(N1, N2);
+    return 0;
+}
