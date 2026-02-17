70d6e2522d8c666730d3c74c3b53bfdf16ea0fd6
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-17 15:36:16 +0530

    que20

---
+++ Assingnment3/que20.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int pos = 1;
+    int inv = 0;
+
+    while (n > 0) {
+        int d = n % 10;
+
+        int place = 1;
+        for (int i = 1; i < d; i++)
+            place = place * 10;
+
+        inv = inv + pos * place;
+
+        pos = pos + 1;
+        n = n / 10;
+    }
+
+    cout << inv;
+}
