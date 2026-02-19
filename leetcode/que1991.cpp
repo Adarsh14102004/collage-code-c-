ee071c7b2c6bb1b5cb1a45644ef517c94ae85d8c
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-19 21:07:02 +0530

    que1991

--- leetcode/que1991.cpp
+++ leetcode/que1991.cpp
@@ -1,1 +1,26 @@
 // same as que no 724
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int totalSum = 0;
+        for (int x : nums) {
+            totalSum += x;
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+            
+            if (leftSum == rightSum) {
+                return i;   // leftmost middleIndex
+            }
+            
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+};
