cd2e31a23595e5611026a666b09d8a08644b759c
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-02-23 21:44:18 +0530

    que2529

--- leetcode/que2529.cpp
+++ leetcode/que2529.cpp
@@ -3,17 +3,32 @@
 #include <algorithm>
 using namespace std;
 
-int maximumCount(vector<int>& nums) {
-    int positiveCount = 0;
-    int negativeCount = 0;
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int positiveCount = 0;
+        int negativeCount = 0;
 
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] > 0) {
-            positiveCount++;
-        } else if (nums[i] < 0) {
-            negativeCount++;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                positiveCount++;
+            } 
+            else if (nums[i] < 0) {
+                negativeCount++;
+            }
         }
-    }
 
-    return max(positiveCount, negativeCount);
+        return max(positiveCount, negativeCount);
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {-2, -1, -1, 1, 2, 3};  // test input
+    int result = obj.maximumCount(nums);
+
+    cout << "Maximum Count (pos vs neg): " << result << endl;
+
+    return 0;
 }
