a0fb9da259f1930c33b0e83be4c8f864468809ed
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-03-07 10:04:59 +0530

    Running Sum of the Array

---
+++ Assingnment4/Running Sum of the Array.cpp
@@ -0,0 +1,22 @@
+//Running Sum of the Array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long arr[1000];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+        cout << sum << " ";
+    }
+
+    return 0;
+}
