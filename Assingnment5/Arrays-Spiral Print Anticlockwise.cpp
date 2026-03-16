37b064c7710de3b73b750ab122359fb6dc70939f
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-03-16 22:39:23 +0530

    Arrays-Wave print Column Wise

--- Assingnment5/Arrays-Spiral Print Anticlockwise.cpp
+++ Assingnment5/Arrays-Spiral Print Anticlockwise.cpp
@@ -1,1 +1,46 @@
 //Arrays-Spiral Print Anticlockwise
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m,n;
+    cin>>m>>n;
+    int a[10][10];
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    int top=0,bottom=m-1,left=0,right=n-1;
+
+    while(top<=bottom && left<=right){
+
+        for(int i=top;i<=bottom;i++){
+            cout<<a[i][left]<<", ";
+        }
+        left++;
+
+        for(int i=left;i<=right;i++){
+            cout<<a[bottom][i]<<", ";
+        }
+        bottom--;
+
+        if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                cout<<a[i][right]<<", ";
+            }
+            right--;
+        }
+
+        if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                cout<<a[top][i]<<", ";
+            }
+            top++;
+        }
+    }
+
+    cout<<"END";
+}
---
+++ Assingnment5/Arrays-Wave print Column Wise.cpp
@@ -0,0 +1,29 @@
+//Arrays-Wave print Column Wise
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int a[10][10];
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    for(int j=0;j<n;j++){
+        if(j%2==0){
+            for(int i=0;i<m;i++){
+                cout<<a[i][j]<<", ";
+            }
+        }else{
+            for(int i=m-1;i>=0;i--){
+                cout<<a[i][j]<<", ";
+            }
+        }
+    }
+
+    cout<<"END";
+}
