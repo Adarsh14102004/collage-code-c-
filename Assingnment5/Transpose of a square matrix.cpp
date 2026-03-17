acb9a5c488a7dd9e83fb09ac238c7a169966bcc4
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-03-17 07:21:50 +0530

    Transpose of a square matrix

--- Assingnment5/Transpose of a square matrix.cpp
+++ Assingnment5/Transpose of a square matrix.cpp
@@ -1,1 +1,23 @@
 //Transpose of a square matrix
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int a[1000][1000];
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
