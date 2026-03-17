e86ec12b8546f357a9327fb7b568fcbbc67f6981
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-03-17 07:20:42 +0530

    Rotate Image!

---
+++ Assingnment5/Rotate Image!.cpp
@@ -0,0 +1,23 @@
+//Rotate Image!
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int a[1000][1000];
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    for(int j=n-1;j>=0;j--){
+        for(int i=0;i<n;i++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
