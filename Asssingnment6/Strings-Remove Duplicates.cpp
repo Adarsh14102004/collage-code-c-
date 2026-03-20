18507347fd7fe79a7b159f53f1258842b8cb2b26
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-03-20 19:44:56 +0530

    Strings-Remove Duplicates

---
+++ Asssingnment6/Strings-Remove Duplicates.cpp
@@ -0,0 +1,20 @@
+//Strings-Remove Duplicates
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    
+    string result = "";
+    result += s[0];
+    
+    for(int i = 1; i < s.size(); i++) {
+        if(s[i] != s[i-1]) {
+            result += s[i];
+        }
+    }
+    
+    cout << result;
+    return 0;
+}
