558d2c89d2be5ad9a4fa14516334e892f3d987c8
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-04-18 21:52:26 +0530

    loutry

--- Lecture_4/loutry.cpp
+++ Lecture_4/loutry.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
-    if (N >= 300 && N <= 460) {
+    if (n >= 300 && n <= 460) {
         cout << "Macbook";
     }
-    else if (N >= 200 && N <= 280) {
+    else if (n >= 200 && n <= 280) {
         cout << "Kurkura";
     }
-    else if (N >= 1100 && N <= 1500) {
+    else if (n >= 1100 && n <= 1500) {
         cout << "Cycle";
     }
-    else if (N >= 50 && N <= 80) {
+    else if (n >= 50 && n <= 80) {
         cout << "Bike";
     }
     else {
