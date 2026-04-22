aaff35cec4cf31cafc5281f9bb2555f0a8317570
Adarsh1402004 <kalmodiyaadarsh@gmail.com>
2026-04-22 19:36:21 +0530

    pattern1

--- Lecture_4/pattern_1.cpp
+++ Lecture_4/pattern_1.cpp
@@ -1,11 +1,10 @@
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n = 5;
+    int N = 5;
 
-    for(int i = 1; i <= n; i++) {
+    for(int i = 1; i <= N; i++) {
         cout << "* ";
     }
 
